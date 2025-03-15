1f9e9d7cde1ebbf1313a03278c48f072b9eb640d
Willian Fernandes Guimaraes <990634@adm.bancoamazonia.sa>
2025-03-14 21:00:32 -0300

    Commit inicial do projeto.

---
+++ src/main/java/com/teste/consultacredito/ConsultaCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.teste.consultacredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaCreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/consultacredito/ConsultaCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.consultacredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
